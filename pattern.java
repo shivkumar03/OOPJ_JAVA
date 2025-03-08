7b070d166f7bfdd20cfa426f38d6cf1b1f4b73e3
shivkumar03 <shivprajapati2060@gmail.com>
2025-03-08 23:17:36 +0530

    JAVA Lab Experiment(4th Sem)

---
+++ pattern.java
@@ -0,0 +1,10 @@
+public class pattern{
+    public static void main(String args[]) {
+        for(int i=0;i<5;i++){
+            for(int j=5-1;j>5;j--){
+                System.out.println("*");
+            }
+            System.out.println("\n");
+        }
+    }
+}
